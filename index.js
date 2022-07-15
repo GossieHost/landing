9e5068e75d1b29c8a2b6937032ad5888b01690ad
BasToTheMax <bas@arnold-linda.nl>
2022-07-15 16:56:17 +0200

    fix assets bug

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const log = debug('APP');
 const rlog = debug('REQUEST');
 
 log('Starting...');
+var c = 0;
 
 const app = express();
 
@@ -16,13 +17,14 @@ log('Port: ' + config.port);
 log('Theme: ' + config.theme);
 
 app.use((req, res, next) => {
-    rlog(`${req.method} ${req.path}`);
+    c++;
+    rlog(`${String(c).padStart(5, ' ')}   -   ${req.method} ${req.path}`);
     return next();
 });
 
 var themeDir = __dirname + `/public/${config.theme}`;
 app.set('views', themeDir);
-app.use('/assets',express.static(themeDir + '/assets'));
+app.use('/assets', express.static(themeDir + '/assets'));
 
 app.get('*', (req, res) => {
     var pa = req.path;
@@ -30,22 +32,24 @@ app.get('*', (req, res) => {
 
     pa = pa.replace('/', '');
 
-    // console.log(pa);
+    console.log(pa);
 
     try {
-    res.render(pa, {
-        config,
-        app,
-        req,
-        res
-    }, (err, html) => {
-        if (err) {
-            return res.render('404', {err});
-        } else {
-            return res.send(html);
-        }
-    });
+        res.render(pa, {
+            config,
+            app,
+            req,
+            res
+        }, (err, html) => {
+            if (err) {
+                res.status(404);
+                return res.render('404', {err});
+            } else {
+                return res.send(html);
+            }
+        });
     } catch(e) {
+        res.status(500);
         res.send('Error: ' + String(e))
     }
     //res.send('try/catch failed?');
